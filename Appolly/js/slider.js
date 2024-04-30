d4e48962a838092e9924c3fc1e0ca5e7bb78534e
Илья Заостровский <112894177+Kot-Kokoss@users.noreply.github.com>
2024-04-30 22:29:47 +0500

    swiper ref

--- Appolly/js/slider.js
+++ Appolly/js/slider.js
@@ -1,3 +1,26 @@
-"use strict"
+// let next_btn = document.querySelector('.slider_btn_right')
+// let prev_btn = document.querySelector('')
+const sampleSlider = new Swiper('.sample-slider', {
+    // if navigation(arrows) is needed
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // if pagination(dots) is needed
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    loop: true,
+    slidesPerView: 5,
+    centeredSlides: true,
+    clickable: true,
+})
 
-console.log(1)
+// const slider = () => {
+//     console.log(12)
+// }
+
+// next_btn.addEventListener('click', () => {
+//     console.log(1)
+//     slider()
+// })
