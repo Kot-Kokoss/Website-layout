b1552db38f1acf53c5eb19fc6dacd6a79b344760
Илья Заостровский <izaostrovskiy@mail.ru>
2024-05-02 00:08:31 +0500

    1

--- Appolly/js/slider.js
+++ Appolly/js/slider.js
@@ -3,13 +3,14 @@
 const sampleSlider = new Swiper('.sample-slider', {
     // if navigation(arrows) is needed
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".slider_btn_right",
+        prevEl: ".slider_btn_left",
     },
     // if pagination(dots) is needed
     pagination: {
         el: '.swiper-pagination',
     },
+    centeredSlides: true,
     loop: true,
     slidesPerView: 5,
     centeredSlides: true,
